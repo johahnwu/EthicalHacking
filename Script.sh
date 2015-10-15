d007e55476092cc57e881fc3f1abe52c9f358936
rosarywang <wangr@mccombs.utexas.edu>
2015-10-14 19:52:05 -0500

    add to script.sh

--- Script.sh
+++ Script.sh
@@ -13,7 +13,28 @@ python PortParser.py > parsedPorts
 
 
 #Banner Grabbing
+output='Step3Result.txt'
 
+if [ -f $output ]; then
+rm $output
+fi
+
+filename='parsedPorts'
+while read p;
+do
+        line=($p)
+        ip=${line[0]}
+        port=${line[1]}
+        nmap -sV $ip -p$port >> $output
+done < $filename
+sed -i '/Starting Nmap/d' ./$output
+sed -i '/Host is up/d' ./$output
+sed -i '/MAC Address/d' ./$output
+sed -i '/Service detection/d' ./$output
+sed -i '/Nmap done:/d' ./$output
+sed -i '/Service Info:/d' ./$output
+sed -i '/Note: /d' ./$output
+sed -i '/^\s*$/d' ./$output
 
 
 
