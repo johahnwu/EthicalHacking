c71e5c1f5f4f21f6cfa0352084fc7ef5739da720
rosarywang <wangr@mccombs.utexas.edu>
2015-10-30 14:47:58 -0500

    step4

--- Script.sh
+++ Script.sh
@@ -43,3 +43,17 @@ sed -i '/^\s*$/d' ./$output
 
 # Vunerability checking
 echo Step 4 checking database for vulnerabilities
+
+filename='cpeResult.txt'
+output='potentialAttacks.txt'
+
+if [[ -f $output ]]; then
+	#statements
+	rm $output
+fi
+
+#python vFeed/vfeedcli.py -u
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	#statements
+	python vFeed/vfeedcli.py -s $line >> $output
+done < $filename
