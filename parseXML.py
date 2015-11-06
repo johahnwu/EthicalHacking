0e094ebb7d804c145d7b9eeac4d52007dce6b521
rosarywang <wangr@mccombs.utexas.edu>
2015-11-06 14:48:43 -0600

    if statement for no exploitable

--- parseXML.py
+++ parseXML.py
@@ -5,7 +5,10 @@ e = xml.etree.ElementTree.parse(sys.argv[1]).getroot()
 cpe = False
 for tag in e.iter('cpe'):
 	cpe = True
-	print(tag.text)
+	if((tag.text).startswith("cpe:/a:",0,7))
+		print(tag.text)
+	else:
+		print("No Exploitable")
 if(not cpe):
 	print("No Exploitable")
 
