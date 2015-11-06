af981bd477bf6ac6da6e4c0c57e154b9cc18a949
rosarywang <wangr@mccombs.utexas.edu>
2015-11-06 15:18:22 -0600

    :

--- parseXML.py
+++ parseXML.py
@@ -5,7 +5,7 @@ e = xml.etree.ElementTree.parse(sys.argv[1]).getroot()
 cpe = False
 for tag in e.iter('cpe'):
 	cpe = True
-	if((tag.text).startswith("cpe:/a:",0,7))
+	if((tag.text).startswith("cpe:/a:",0,7)):
 		print(tag.text)
 	else:
 		print("No Exploitable")
